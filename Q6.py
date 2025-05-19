d852017fdd8487e501ab058540f3c6a0129bb35a
keiceey <kiptoo.caxton@gmail.com>
2025-05-19 21:51:55 +0300

    final commit

--- Q6.py
+++ Q6.py
@@ -1,13 +1,12 @@
-# Q6. Write a Python program that takes 5 values from the user and calculates the average of those values.
+#The code takes 5 values from the user, populating them as an array and calculates the average of those values.
 while True:
     try:
-        Val = int(input("Please input 5 values:"))
-        if len(Val) != 5:
-            print("Invalid input! Please enter 5 values.")
-        else:
-            average = sum(Val) / len(Val)
-            print(average)
-            break
+        print("Please input five values.")
+        Val = [int(input(f"{i+1}: ")) for i in range(5)]
     except ValueError:
-        print("Input should be numerical!")
-        continue   
+        print("Invalid input! Please enter numeric values.")
+        continue  
+    print(f"Your array of values is: {Val}")
+    average = sum(Val) / len(Val)
+    print(f"Average of the Values is :{average}")
+    break
