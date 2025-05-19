457d2e72b22c239b92bb00b7bc5ffee477355c13
keiceey <kiptoo.caxton@gmail.com>
2025-05-19 21:13:37 +0300

    2nd commit

--- Q2.py
+++ Q2.py
@@ -4,7 +4,7 @@ while True:
     try:
         Radius = float(input("Please input the radius:"))
         if Radius <= 0:
-            print("Invalid input!")
+            print("Invalid input! Radius should be a positive number.")
         else:
             Volume = (4/3) * math.pi * (Radius ** 3)
             print(f"Given radius of {Radius}, the volume of the sphere is {Volume:.2f}")
